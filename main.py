38df38d3692dd1e466974a74ff07c461dd1e6fc7
MichaelKap <kapkanetsmihail@gmail.com>
2023-02-05 15:22:56 +0200

    new messeg

--- main.py
+++ main.py
@@ -4,26 +4,4 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-class Student:
-    def __init__(self,hright = 160):
-
-        self.height = height
-nick = Student()
-print(nick.height)
-first_student = Student()
-
-nick = Student()
-kate = Student(neight=170)
-print(nick.neight)
-print(kate.neight)
+def print (Hello,Teache)
